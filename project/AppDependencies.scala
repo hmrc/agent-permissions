7703f080e3427d2cbf2693d7207727975320eb32
geoffwatson <9043337+geoffreywatson@users.noreply.github.com>
2022-07-28 15:44:24 +0100

    bump mongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "6.4.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.67.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.68.0",
     "uk.gov.hmrc"             %% "agent-mtd-identifiers"      % "0.45.0-play-28",
     "uk.gov.hmrc"             %% "agent-kenshoo-monitoring"   % "4.8.0-play-28"
   )
