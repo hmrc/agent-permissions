5dd717447e7ba1fc1f07e57392c3f6219c05d35d
Unmesh Malvankar <unmesh.malvankar1@digital.hmrc.gov.uk>
2022-12-08 09:20:50 +0000

    [APB-6618][UM] Bumps up maxClientCountAllowed to 10000 (#63)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,17 +6,16 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "7.7.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.73.0",
-    "uk.gov.hmrc"             %% "agent-mtd-identifiers"      % "0.47.0-play-28",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "7.12.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.74.0",
+    "uk.gov.hmrc"             %% "agent-mtd-identifiers"      % "0.50.0-play-28",
     "uk.gov.hmrc"             %% "agent-kenshoo-monitoring"   % "4.8.0-play-28",
-    "uk.gov.hmrc"             %% "crypto"                     % "7.2.0",
-    "uk.gov.hmrc"             %% "crypto-json-play-28"        % "7.2.0"
+    "uk.gov.hmrc"             %% "crypto-json-play-28"        % "7.3.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.7.0"             % "test, it",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.73.0"            % "test, it",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.12.0"             % "test, it",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.74.0"            % "test, it",
     "org.scalamock"           %% "scalamock"                  % "5.2.0"             % "test, it"
   )
 }
