4cbffcfdb241cd521c99d0a93cecb63e570efac7
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2023-01-10 17:02:18 +0000

    APB-6728 [CS] bump agent-mtd-identifiers

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,13 +8,13 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "7.12.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.74.0",
-    "uk.gov.hmrc"             %% "agent-mtd-identifiers"      % "0.52.0-play-28",
+    "uk.gov.hmrc"             %% "agent-mtd-identifiers"      % "0.53.0-play-28",
     "uk.gov.hmrc"             %% "agent-kenshoo-monitoring"   % "4.8.0-play-28",
     "uk.gov.hmrc"             %% "crypto-json-play-28"        % "7.3.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.12.0"             % "test, it",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.12.0"            % "test, it",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.74.0"            % "test, it",
     "org.scalamock"           %% "scalamock"                  % "5.2.0"             % "test, it"
   )
