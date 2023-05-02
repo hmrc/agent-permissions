3383c24ef080a9cd0d9a8fc1fd4741f1a4e2409e
max-hmrc <max.ashton@digital.hmrc.gov.uk>
2023-05-02 11:54:21 +0100

    APB-7178 upgrade to scala 2.13

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,16 +9,16 @@ object AppDependencies {
   private val bootstrapVer: String = "7.13.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapVer,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % mongoVer,
-    "uk.gov.hmrc"             %% "agent-mtd-identifiers"      % "1.2.0",
-    "uk.gov.hmrc"             %% "agent-kenshoo-monitoring"   % "5.3.0",
-    "uk.gov.hmrc"             %% "crypto-json-play-28"        % "7.3.0"
+    "uk.gov.hmrc"          %% "bootstrap-backend-play-28"  % bootstrapVer,
+    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-play-28"         % mongoVer,
+    "uk.gov.hmrc"          %% "agent-mtd-identifiers"      % "1.2.0",
+    "uk.gov.hmrc"          %% "agent-kenshoo-monitoring"   % "5.3.0",
+    "uk.gov.hmrc"          %% "crypto-json-play-28"        % "7.3.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.13.0"            % "test, it",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.74.0"            % "test, it",
-    "org.scalamock"           %% "scalamock"                  % "5.2.0"             % "test, it"
+    "uk.gov.hmrc"          %% "bootstrap-test-play-28"     % bootstrapVer   % "test, it",
+    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-28"    % mongoVer       % "test, it",
+    "org.scalamock"        %% "scalamock"                  % "5.2.0"        % "test, it"
   )
 }
