c66f341ba299b7dbae8299ef010593d04a48c51a
max-hmrc <max.ashton@digital.hmrc.gov.uk>
2023-05-02 20:00:02 +0100

    APB-7178 exclude bootstrap-backend-play-28 from kenshoo import

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,12 +7,13 @@ object AppDependencies {
 
   private val mongoVer: String = "1.1.0"
   private val bootstrapVer: String = "7.15.0"
+  private val bootstrapBackend = "bootstrap-backend-play-28"
 
   val compile = Seq(
-    "uk.gov.hmrc"          %% "bootstrap-backend-play-28"  % bootstrapVer,
+    "uk.gov.hmrc"          %% bootstrapBackend             % bootstrapVer,
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-play-28"         % mongoVer,
     "uk.gov.hmrc"          %% "agent-mtd-identifiers"      % "1.2.0",
-    "uk.gov.hmrc"          %% "agent-kenshoo-monitoring"   % "5.3.0",
+    "uk.gov.hmrc"          %% "agent-kenshoo-monitoring"   % "5.3.0" exclude("uk.gov.hmrc", bootstrapBackend),
     "uk.gov.hmrc"          %% "crypto-json-play-28"        % "7.3.0"
   )
 
