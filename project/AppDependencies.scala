38a5e805122e1a7f60687040a12f6cffb81f21fb
mrolfe88 <52818407+mrolfe88@users.noreply.github.com>
2025-07-22 15:26:45 +0100

    [APB-9704] Bump bootstrap version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val mongoVer: String = "2.6.0"
-  private val bootstrapVer: String = "9.13.0"
+  private val bootstrapVer: String = "9.16.0"
   private val bootstrapBackend = "bootstrap-backend-play-30"
 
   val compile: Seq[ModuleID] = Seq(
