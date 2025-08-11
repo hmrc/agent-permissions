45197989229d3684163dea6f93fa860d79a29db4
geoffreywatson <9043337+geoffreywatson@users.noreply.github.com>
2025-08-11 13:26:37 +0100

    pr bot suggestions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,15 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoVer: String = "2.6.0"
-  private val bootstrapVer: String = "9.16.0"
+  private val mongoVer: String = "2.7.0"
+  private val bootstrapVer: String = "9.19.0"
   private val bootstrapBackend = "bootstrap-backend-play-30"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"          %% bootstrapBackend             % bootstrapVer,
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-play-30"         % mongoVer,
     "uk.gov.hmrc"          %% "agent-mtd-identifiers"      % "2.2.0",
-    "uk.gov.hmrc"          %% "crypto-json-play-30"        % "8.2.0"
+    "uk.gov.hmrc"          %% "crypto-json-play-30"        % "8.3.0"
   )
 
   val test: Seq[ModuleID] = Seq(
