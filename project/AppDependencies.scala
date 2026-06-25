270a57c804eb95e91da22fd74abe9482b86bfb72
raw1697 <44207483+raw1697@users.noreply.github.com>
2026-06-25 16:05:07 +0100

    APB-11675 - Address dependency vulnerabilities (#156)
    * APB-11675 - Address dependency vulnerabilities
    
    * APB-11675 - Address dependency vulnerabilities

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,21 +2,21 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoVer: String = "2.7.0"
-  private val bootstrapVer: String = "10.1.0"
-  private val bootstrapBackend = "bootstrap-backend-play-30"
+  private val mongoVer: String = "2.12.0"
+  private val bootstrapVer: String = "10.7.0"
+  private val playVer: String = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% bootstrapBackend      % bootstrapVer,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"  % mongoVer,
-    "uk.gov.hmrc"       %% "crypto-json-play-30" % "8.3.0",
-    "uk.gov.hmrc"       %% "domain-play-30"      % "11.0.0"
+    "uk.gov.hmrc"       %% s"bootstrap-backend-$playVer"      % bootstrapVer,
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVer"  % mongoVer,
+    "uk.gov.hmrc"       %% s"crypto-json-$playVer" % "8.4.0",
+    "uk.gov.hmrc"       %% s"domain-$playVer"      % "11.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVer % Test,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % mongoVer     % Test,
-    "org.scalamock"     %% "scalamock"               % "7.4.1"      % Test,
-    "org.scalacheck"    %% "scalacheck"              % "1.18.1"     % Test
-  )
+    "uk.gov.hmrc"       %% s"bootstrap-test-$playVer"  % bootstrapVer,
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test-$playVer" % mongoVer,
+    "org.scalamock"     %% "scalamock"               % "7.5.5",
+    "org.scalacheck"    %% "scalacheck"              % "1.18.1"
+  ).map(_ % Test)
 }
